dfcb3c0b25afc9876e39344278a25b262f733772
Kevin Washington <kevw12188@gmail.com>
2014-10-07 17:43:45 -0400

    example finished

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-var app = angular.module('app', [])
+var app = angular.module('app', ['ngRoute'])
+
+app.controller('testController', function($scope){
+
+	$scope.controller = 'testController';
+})
 
 
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/',
+		{
+			template: '<div>This is the template from {{controller}}</div>',
+			controller: 'testController',
+			resolve: {
+				homeView: function($q, $timeout){
+					
+					var deferobj = $q.defer();
+
+					$timeout(function(){
+						deferobj.resolve();
+					}, 3000);
+			
+					return deferobj.promise;
+				}
+			}
+
+
+		});
+});
